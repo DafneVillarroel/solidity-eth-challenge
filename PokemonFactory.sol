4bb38c36fa3631cff24e8dcc161b9e7547d50469
DafneVillarroel <68930845+DafneVillarroel@users.noreply.github.com>
2022-10-12 18:48:57 -0300

    Challenge finalized

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -7,29 +7,61 @@ contract PokemonFactory {
   struct Pokemon {
     uint id;
     string name;
+    Abilities[] ability;
+    Type[] types;
+    Weakness[] weaknesses;
+  }
+  struct Weakness{
+    string name;
   }
 
-    Pokemon[] private pokemons;
+  struct Type{
+    string name;
+  }
 
-    mapping (uint => address) public pokemonToOwner;
-    mapping (address => uint) ownerPokemonCount;
+  struct Abilities{
+    string nameAbility;
+    string description;
+  }
 
-     function createPokemon (string memory _name, uint _id) public {
-        pokemons.push(Pokemon(_id, _name));
-        pokemonToOwner[_id] = msg.sender;
+  event eventNewPokemon(uint id, string name);
+
+  Pokemon[] private pokemons;
+
+  mapping (uint => address) public pokemonToOwner;
+  mapping (address => uint) ownerPokemonCount;
+
+  function createPokemon (string memory _name, uint _id, string memory _nameAbility, string memory _description, string memory _weaknessName, string memory _typeName) public {
+        require(_id > 0 , "Add a pokemon with id greater than 0");
+        require( bytes(_name).length > 2 || bytes(_name).length == 0, "Insert a name greater than 2 characters");
+        pokemons.push();
+        uint _index = pokemons.length -1;
+        pokemons[_index].id = _index;
+        pokemons[_index].name = _name;
+        addAbility(_index, _nameAbility, _description);
+        addType(_index, _typeName);
+        addWeakness(_index, _weaknessName);
+        pokemonToOwner[_index] = msg.sender;
         ownerPokemonCount[msg.sender]++;
+        emit eventNewPokemon(_id, _name);
     }
 
-    function getAllPokemons() public view returns (Pokemon[] memory) {
-      return pokemons;
+    function addAbility(uint _id, string memory _nameAbility, string memory _description) public  {
+     pokemons[_id].ability.push(Abilities(_nameAbility, _description));
+    }
+    
+    function addType(uint _id, string memory _typeName) public {
+    pokemons[_id].types.push(Type(_typeName));
+    }
+  
+   function addWeakness(uint _id, string memory _weaknessName) public {
+    pokemons[_id].weaknesses.push(Weakness(_weaknessName));
+    }
+   
+   function getAllPokemons() public view returns (Pokemon[] memory) {
+     return pokemons;
     }
 
 
-    function getResult() public pure returns(uint product, uint sum){
-      uint a = 1; 
-      uint b = 2;
-      product = a * b;
-      sum = a + b; 
-   }
 
 }
